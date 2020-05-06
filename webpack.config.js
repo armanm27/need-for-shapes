b59216608af565baf70032faf57c12f1e79ba77d
taylorbeckett <56843962+taylorbeckett@users.noreply.github.com>
2020-05-05 20:07:03 -0400

    Add support for scoreboard/timer

--- webpack.config.js
+++ webpack.config.js
@@ -46,6 +46,8 @@ module.exports = {
             lights$: path.resolve(__dirname, 'src/components/lights'),
             objects$: path.resolve(__dirname, 'src/components/objects'),
             scenes$: path.resolve(__dirname, 'src/components/scenes'),
+            scoreboard$: path.resolve(__dirname, 'src/components/scoreboard'),
+            timer$: path.resolve(__dirname, 'src/components/timer'),
         },
     },
     plugins: [
